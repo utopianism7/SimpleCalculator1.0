496fe68aa109d3dd328fc93a03d57e8ad4acb17a
jarvis.zeng <gogo@jarviszengdeMacBook-Pro.local>
2015-06-19 18:14:48 +0800

    Initial Commit

---
+++ SimpleCalculator1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCalculator1.0
+//
+//  Created by jarvis.zeng on 15/6/19.
+//  Copyright (c) 2015年 jarvis.zeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SimpleCalculator1.0Tests/SimpleCalculator1_0Tests.swift
@@ -0,0 +1,36 @@
+//
+//  SimpleCalculator1_0Tests.swift
+//  SimpleCalculator1.0Tests
+//
+//  Created by jarvis.zeng on 15/6/19.
+//  Copyright (c) 2015年 jarvis.zeng. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SimpleCalculator1_0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
